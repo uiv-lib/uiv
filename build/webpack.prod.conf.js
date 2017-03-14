9f4f22ec018e3e8366459457ca5e3471ef2a3ebc
wxsm <edisond@qq.com>
2017-03-14 10:30:11 +0800

    update document

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -22,7 +22,7 @@ var webpackConfig = merge(baseWebpackConfig, {
   },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
-    publicPath: './',
+    publicPath: 'https://wxsm.space/uiv/',
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
