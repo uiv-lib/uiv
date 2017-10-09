5ee59867456ff74cc18e3db7e52342618c980e8d
wxsm <edisond@qq.com>
2017-10-09 11:25:51 +0800

    Document update: new language

--- src/docs/pages/I18n.vue
+++ src/docs/pages/I18n.vue
@@ -30,6 +30,7 @@ let appLocale = Object.assign({}, uivLocale, {
           <li>cs-CZ</li>
           <li>en-US</li>
           <li>fr-FR</li>
+          <li>nb-NO</li>
           <li>pt-BR</li>
           <li>zh-CN</li>
         </ul>
