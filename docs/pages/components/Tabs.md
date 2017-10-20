e1615d64e3dc2cae923ed6e5137ab6eb90c826be
wxsm <edisond@qq.com>
2017-10-20 15:45:21 +0800

    [Document] Update Tabs doc

--- docs/pages/components/Tabs.md
+++ docs/pages/components/Tabs.md
@@ -5,47 +5,108 @@
 ## Example
 
 ```html
+<tabs>
+  <tab title="Home">
+    <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+  </tab>
+  <tab title="Profile">
+    <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+  </tab>
+  <tab title="@vue" group="Dropdown">
+    <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+  </tab>
+  <tab title="@bootstrap" group="Dropdown">
+    <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.</p>
+  </tab>
+</tabs>
+<!-- Live demo -->
+```
+
+## Disable Tabs
+
+Add `disabled` to `<tab>` to disable it.
+
+```html
+<tabs>
+  <tab title="Home">
+    <p>Home tab.</p>
+  </tab>
+  <tab title="Profile" disabled>
+    <p>Profile tab.</p>
+  </tab>
+  <tab title="@vue" group="Dropdown">
+    <p>@vue tab.</p>
+  </tab>
+  <tab title="@bootstrap" group="Dropdown" disabled>
+    <p>@bootstrap tab.</p>
+  </tab>
+</tabs>
+<!-- Live demo -->
+```
+
+## Pull Right
+
+Add `pull-right` to `<tab>` to pull it right. A grouped tab will be pulled to right if one of its sub-tabs has this prop set to `true`.
+
+```html
+<tabs>
+  <tab title="Home">
+    <p>Home tab.</p>
+  </tab>
+  <tab title="Profile">
+    <p>Profile tab.</p>
+  </tab>
+  <tab title="@vue" group="Dropdown" pull-right>
+    <p>@vue tab.</p>
+  </tab>
+  <tab title="@bootstrap" group="Dropdown">
+    <p>@bootstrap tab.</p>
+  </tab>
+</tabs>
+<!-- Live demo -->
+```
+
+## Justified Style
+
+Add `justified` to `<tabs>` to use justified style.
+
+```html
+<tabs justified>
+  <tab title="Home">
+    <p>Home tab.</p>
+  </tab>
+  <tab title="Profile">
+    <p>Profile tab.</p>
+  </tab>
+  <tab title="Others">
+    <p>Others tab.</p>
+  </tab>
+</tabs>
+<!-- Live demo -->
+```
+
+## With Callbacks
+
+```html
 <template>
-  <tabs ref="tabComponent" :justified="justified" v-model="tabIndex" @change="afterTabActive">
-    <tab title="Tab 1">
-      <p>This is tab 1.</p>
+  <tabs @change="onChange">
+    <tab title="Home">
+      <p>Home tab.</p>
     </tab>
-    <tab title="Tab 2">
-      <p>Tab 2 goes here.</p>
-    </tab>
-    <tab :title="thirdTabDisabled?'Tab 3 (Disabled)':'Tab 3 (Enabled)'" :disabled="thirdTabDisabled">
-      <p>This tab can be enable / disable.</p>
-    </tab>
-    <tab title="Tab in Group 1" group="Tab Group">
-      <p>This is Tab in group 1.</p>
-    </tab>
-    <tab title="Tab in Group 2" group="Tab Group">
-      <p>This is Tab in group 2.</p>
+    <tab title="Profile">
+      <p>Profile tab.</p>
     </tab>
     <tab title="<i class='glyphicon glyphicon-bell'></i> Alert!" :html-title="true">
-      <p>This tab has a HTML and callback function.</p>
+      <p>This tab has HTML title and callback function!</p>
     </tab>
   </tabs>
-  <hr/>
-  <h4>Settings</h4>
-  <button type="button" id="tabs-btn-1" class="btn btn-default" @click="tabIndex = 0">Active Tab 1</button>
-  <button type="button" id="tabs-btn-2" class="btn btn-default" @click="tabIndex = 1">Active Tab 2</button>
-  <button type="button" id="tabs-btn-3" class="btn btn-default" @click="thirdTabDisabled=!thirdTabDisabled">Enable / Disable Tab 3</button>
-  <button type="button" id="tabs-btn-4" class="btn btn-default" @click="justified=!justified">Justified Style</button>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        thirdTabDisabled: true,
-        justified: false,
-        tabIndex: 0
-      }
-    },
     methods: {
-      afterTabActive (index) {
-        if (index === 5) {
+      onChange (index) {
+        if (index === 2) {
           window.alert('You clicked on a tab that has callback function!')
         }
       }
@@ -60,11 +121,11 @@
 
 ```html
 <tabs>
-  <tab title="Tab 1">
-    <p>This is tab 1.</p>
+  <tab title="Home">
+    <p>Home tab.</p>
   </tab>
-  <tab title="Tab 2">
-    <p>Tab 2 goes here.</p>
+  <tab title="Profile">
+    <p>Profile tab.</p>
   </tab>
   <form slot="nav-right">
     <select class="form-control" style="display: inline-block;width: auto">
@@ -207,16 +268,9 @@
 <!-- Live demo script
 <script>
   export default {
-    data () {
-      return {
-        thirdTabDisabled: true,
-        justified: false,
-        tabIndex: 0
-      }
-    },
     methods: {
-      afterTabActive (index) {
-        if (index === 5) {
+      onChange (index) {
+        if (index === 2) {
           window.alert('You clicked on a tab that has callback function!')
         }
       }
--- test/unit/specs/Tabs.spec.js
+++ test/unit/specs/Tabs.spec.js
@@ -1,25 +1,23 @@
 import Vue from 'vue'
+import $ from 'jquery'
 import Tabs from '@src/components/tabs/Tabs.vue'
 import Tab from '@src/components/tabs/Tab.vue'
 import TabsDoc from '@docs/pages/components/Tabs.md'
 import utils from '../utils'
 
 describe('Tabs', () => {
-  let root
+  let vm
+  let $el
 
   beforeEach(() => {
-    root = new Vue({
-      template: '<TabsDoc ref="doc"/>',
-      components: {TabsDoc}
-    })
+    let Constructor = Vue.extend(TabsDoc)
+    vm = new Constructor().$mount()
+    $el = $(vm.$el)
   })
 
   afterEach(() => {
-    try {
-      root.$destroy()
-    } catch (err) {
-      // Silent
-    }
+    vm.$destroy()
+    $el.remove()
   })
 
   it('should not be able to work if not using <tabs><tab>...</tab></tabs>', () => {
@@ -42,13 +40,14 @@ describe('Tabs', () => {
       })
       vm.$mount()
       sinon.assert.called(spy)
+      $(vm.$el).remove()
       vm.$destroy()
     } finally {
       window.console.error = _error
     }
   })
 
-  it('should be ok if not <tab> present in <tabs>', () => {
+  it('should be ok if no <tab> present in <tabs>', () => {
     let res = Vue.compile('<tabs>{{msg}}</tabs>')
     let vm = new Vue({
       data () {
@@ -61,156 +60,134 @@ describe('Tabs', () => {
       staticRenderFns: res.staticRenderFns
     })
     vm.$mount()
+    $(vm.$el).remove()
     vm.$destroy()
   })
 
-  it('should be able to render first tab on open', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let activeTab = vm.$el.querySelector('.nav-tabs').querySelectorAll('.active')
-      expect(activeTab.length).to.equal(1)
-      expect(activeTab[0].querySelector('a').textContent).to.equal('Tab 1')
-      let activeContent = vm.$el.querySelector('.tab-content').querySelectorAll('.tab-pane.active')
-      expect(activeContent.length).to.equal(1)
-      expect(activeContent[0].querySelector('p').textContent).to.equal('This is tab 1.')
-      done()
-    })
+  it('should be able to use with v-model', async () => {
+    let res = Vue.compile('<tabs v-model="index"><tab title="tab0">tab0</tab><tab title="tab1">tab1</tab></tabs>')
+    let vm = new Vue({
+      data () {
+        return {
+          index: 1
+        }
+      },
+      components: {Tab, Tabs},
+      render: res.render,
+      staticRenderFns: res.staticRenderFns
+    }).$mount()
+    let $el = $(vm.$el)
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(0)
+    let content = $el.find('.tab-content').get(0)
+    let activeTab = nav.querySelectorAll('.active')
+    expect(activeTab.length).to.equal(1)
+    expect(activeTab[0].querySelector('a').textContent).to.equal('tab1')
+    let activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].textContent).to.contain('tab1')
+    utils.triggerEvent(nav.querySelectorAll('li')[0].querySelector('a'), 'click')
+    await vm.$nextTick()
+    activeTab = nav.querySelectorAll('.active')
+    expect(activeTab.length).to.equal(1)
+    expect(activeTab[0].querySelector('a').textContent).to.equal('tab0')
+    activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].textContent).to.contain('tab0')
+    vm.$destroy()
+    $el.remove()
   })
 
-  it('should be able to open correct tab content after click on tab nav', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let tab = vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[1].querySelector('a')
-      utils.triggerEvent(tab, 'click')
-      // Double click should be fine
-      utils.triggerEvent(tab, 'click')
-      vm.$nextTick(() => {
-        let activeTab = vm.$el.querySelector('.nav-tabs').querySelectorAll('.active')
-        expect(activeTab.length).to.equal(1)
-        expect(activeTab[0].querySelector('a').textContent).to.equal('Tab 2')
-        let activeContent = vm.$el.querySelector('.tab-content').querySelectorAll('.tab-pane.active')
-        expect(activeContent.length).to.equal(1)
-        expect(activeContent[0].querySelector('p').textContent).to.equal('Tab 2 goes here.')
-        done()
-      })
-    })
+  it('should be able to render first tab on open', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(0)
+    let content = $el.find('.tab-content').get(0)
+    let activeTab = nav.querySelectorAll('.active')
+    expect(activeTab.length).to.equal(1)
+    expect(activeTab[0].querySelector('a').textContent).to.equal('Home')
+    let activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].querySelector('p').textContent).to.contain('Raw denim you probably haven')
   })
 
-  it('should be able to open tab 2 on method called', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      utils.triggerEvent(vm.$el.querySelector('#tabs-btn-2'), 'click')
-      vm.$nextTick(() => {
-        let activeTab = vm.$el.querySelector('.nav-tabs').querySelectorAll('.active')
-        expect(activeTab.length).to.equal(1)
-        expect(activeTab[0].querySelector('a').textContent).to.equal('Tab 2')
-        let activeContent = vm.$el.querySelector('.tab-content').querySelectorAll('.tab-pane.active')
-        expect(activeContent.length).to.equal(1)
-        expect(activeContent[0].querySelector('p').textContent).to.equal('Tab 2 goes here.')
-        done()
-      })
-    })
+  it('should be able to open correct tab content after click on tab nav', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(0)
+    let content = $el.find('.tab-content').get(0)
+    let tab = nav.querySelectorAll('li')[1].querySelector('a')
+    utils.triggerEvent(tab, 'click')
+    await vm.$nextTick()
+    // Double click should be fine
+    utils.triggerEvent(tab, 'click')
+    await vm.$nextTick()
+    let activeTab = nav.querySelectorAll('.active')
+    expect(activeTab.length).to.equal(1)
+    expect(activeTab[0].querySelector('a').textContent).to.equal('Profile')
+    let activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].querySelector('p').textContent).to.contain('Food truck fixie locavore, accusamus mcsw')
   })
 
-  it('should not be able to select disabled tab 3', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let tab3 = vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[2]
-      expect(tab3.className).to.equal('disabled')
-      expect(tab3.querySelector('a').textContent).to.equal('Tab 3 (Disabled)')
-      utils.triggerEvent(tab3.querySelector('a'), 'click')
-      vm.$nextTick(() => {
-        expect(tab3.className).to.equal('disabled')
-        expect(tab3.querySelector('a').textContent).to.equal('Tab 3 (Disabled)')
-        let activeContent = vm.$el.querySelector('.tab-content').querySelectorAll('.tab-pane.active')
-        expect(activeContent.length).to.equal(1)
-        expect(activeContent[0].querySelector('p').textContent).to.equal('This is tab 1.')
-        done()
-      })
-    })
+  it('should not be able to select disabled tab', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(1)
+    let content = $el.find('.tab-content').get(1)
+    // In nav
+    let tab1 = nav.querySelectorAll('li')[1]
+    expect(tab1.className).to.equal('disabled')
+    utils.triggerEvent(tab1.querySelector('a'), 'click')
+    await vm.$nextTick()
+    expect(tab1.className).to.equal('disabled')
+    let activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].querySelector('p').textContent).to.contain('Home tab')
+    // In dropdown
+    let tab2 = nav.querySelector('.dropdown').querySelectorAll('li')[1]
+    expect(tab2.className).to.equal('disabled')
+    utils.triggerEvent(tab2.querySelector('a'), 'click')
+    await vm.$nextTick()
+    expect(tab2.className).to.equal('disabled')
+    activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].querySelector('p').textContent).to.contain('Home tab')
   })
 
-  it('should be able to enable / disable tab 3', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let enableBtn = vm.$el.querySelector('#tabs-btn-3')
-      utils.triggerEvent(enableBtn, 'click')
-      vm.$nextTick(() => {
-        let tab3 = vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[2]
-        expect(tab3.className).to.equal('')
-        expect(tab3.querySelector('a').textContent).to.equal('Tab 3 (Enabled)')
-        utils.triggerEvent(enableBtn, 'click')
-        vm.$nextTick(() => {
-          let tab3 = vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[2]
-          expect(tab3.className).to.equal('disabled')
-          expect(tab3.querySelector('a').textContent).to.equal('Tab 3 (Disabled)')
-          done()
-        })
-      })
-    })
+  it('should be able to render HTML title', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(4)
+    let tab = nav.querySelectorAll('li')[2]
+    expect(tab.querySelector('i')).to.exist
   })
 
-  it('should be able to render HTML title on tab 6', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let tab = vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[6]
-      expect(tab.querySelector('i')).to.exist
-      done()
-    })
+  it('should be able to run callback function', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(4)
+    let _savedAlert = window.alert
+    window.alert = () => {
+      // Silent to remove out logs in terminal
+    }
+    let spy = sinon.spy(window, 'alert')
+    utils.triggerEvent(nav.querySelectorAll('li')[2].querySelector('a'), 'click')
+    await vm.$nextTick()
+    sinon.assert.called(spy)
+    window.alert = _savedAlert
   })
 
-  it('should be able to run callback after click on tab 6', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let _savedAlert = window.alert
-      window.alert = () => {
-        // Silent to remove out logs in terminal
-      }
-      let spy = sinon.spy(window, 'alert')
-      utils.triggerEvent(vm.$el.querySelector('.nav-tabs').querySelectorAll('li')[6].querySelector('a'), 'click')
-      vm.$nextTick(() => {
-        sinon.assert.called(spy)
-        window.alert = _savedAlert
-        done()
-      })
-    })
-  })
-
-  it('should be able to open grouped tab 5', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let tab5 = vm.$el.querySelector('.nav-tabs').querySelector('li.dropdown')
-      utils.triggerEvent(tab5.querySelectorAll('a')[0], 'click')
-      vm.$nextTick(() => {
-        expect(tab5.querySelector('.dropdown-menu')).to.exist
-        expect(tab5.className).to.contain('dropdown')
-        expect(tab5.className).to.contain('open')
-        utils.triggerEvent(tab5.querySelector('.dropdown-menu').querySelector('li').querySelector('a'), 'click')
-        vm.$nextTick(() => {
-          expect(tab5.className).to.contain('active')
-          let activeContent = vm.$el.querySelector('.tab-content').querySelectorAll('.tab-pane.active')
-          expect(activeContent.length).to.equal(1)
-          expect(activeContent[0].querySelector('p').textContent).to.equal('This is Tab in group 1.')
-          done()
-        })
-      })
-    })
-  })
-
-  it('should be able to use without v-model', (done) => {
-    let vm = root.$mount().$refs.doc
-    vm.$nextTick(() => {
-      let tabs = vm.$el.querySelectorAll('.nav-tabs')[1]
-      utils.triggerEvent(tabs.querySelectorAll('li')[1].querySelector('a'), 'click')
-      vm.$nextTick(() => {
-        let activeTab = tabs.querySelectorAll('.active')
-        expect(activeTab.length).to.equal(1)
-        expect(activeTab[0].querySelector('a').textContent).to.equal('Tab 2')
-        let activeContent = vm.$el.querySelectorAll('.tab-content')[1].querySelectorAll('.tab-pane.active')
-        expect(activeContent.length).to.equal(1)
-        expect(activeContent[0].querySelector('p').textContent).to.equal('Tab 2 goes here.')
-        done()
-      })
-    })
+  it('should be able to open grouped tab', async () => {
+    await vm.$nextTick()
+    let nav = $el.find('.nav-tabs').get(0)
+    let content = $el.find('.tab-content').get(0)
+    let tab5 = nav.querySelector('li.dropdown')
+    utils.triggerEvent(tab5.querySelectorAll('a')[0], 'click')
+    await vm.$nextTick()
+    expect(tab5.querySelector('.dropdown-menu')).to.exist
+    expect(tab5.className).to.contain('dropdown')
+    expect(tab5.className).to.contain('open')
+    utils.triggerEvent(tab5.querySelector('.dropdown-menu').querySelector('li').querySelector('a'), 'click')
+    await vm.$nextTick()
+    expect(tab5.className).to.contain('active')
+    let activeContent = content.querySelectorAll('.tab-pane.active')
+    expect(activeContent.length).to.equal(1)
+    expect(activeContent[0].querySelector('p').textContent).to.contain('Etsy mixtape wayfarers')
   })
 })
