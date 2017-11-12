a1a0887e299921aa35abbb5f21c3663eb1ab7c30
wxsm <edisond@qq.com>
2017-11-12 15:27:18 +0800

    Update doc logo file

--- docs/components/architecture/Logo.vue
+++ docs/components/architecture/Logo.vue
@@ -1,60 +1,31 @@
-<template>
-  <div class="logo" :style="logoStyles">
-    <img class="vue-logo" :style="vStyles" src="../../assets/img/v-logo.png">
-    <img class="bootstrap-logo" :style="bStyles" src="../../assets/img/b-logo.png">
-  </div>
-</template>
-
 <script>
   export default {
+    functional: true,
+    render (h, {props}) {
+      return h('img', {
+        class: 'uiv-logo',
+        style: {
+          height: 150 * props.scale + 'px',
+          width: 195 * props.scale + 'px'
+        },
+        attrs: {
+          src: '/static/logo.png'
+        }
+      })
+    },
     props: {
       scale: {
         type: Number,
         default: 1
-      }
-    },
-    computed: {
-      logoStyles () {
-        return {
-          height: 100 * this.scale + 'px',
-          width: 210 * this.scale + 'px'
-        }
-      },
-      vStyles () {
-        return {
-          height: 100 * this.scale + 'px'
-        }
-      },
-      bStyles () {
-        return {
-          height: 60 * this.scale + 'px',
-          top: 20 * this.scale + 'px'
-        }
       }
     }
   }
 </script>
 
 <style lang="less" rel="stylesheet/less" scoped>
-  .logo {
-    position: relative;
-    height: 100px;
-    width: 210px;
+  .uiv-logo {
     margin: 0 auto;
-
-    .vue-logo {
-      position: absolute;
-      height: 100px;
-      top: 0;
-      left: 20%;
-    }
-
-    .bootstrap-logo {
-      position: absolute;
-      height: 60px;
-      top: 20px;
-      left: 50%;
-      opacity: .8;
-    }
+    display: block;
+    position: relative;
   }
 </style>
