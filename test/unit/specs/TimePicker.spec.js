ef70fbb72e14ba605e825fa47eb1319f458b9782
francoismassart <francois.massart@gmail.com>
2017-11-14 06:58:57 +0100

    REFACT better names for variables in tests

--- test/unit/specs/TimePicker.spec.js
+++ test/unit/specs/TimePicker.spec.js
@@ -37,11 +37,11 @@ describe('TimePicker', () => {
     expect(meridianTextAfterTwoClick).to.equal('AM')
   })
 
-  it('should be able to add hours', async () => {
+  it('should be able to add hour(s)', async () => {
     const _vm = vm.$refs['time-picker-example']
     await vm.$nextTick()
-    const hourInput = _vm.$el.querySelectorAll('input')[0]
-    const beforeText = hourInput.value
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const beforeValue = parseInt(hoursInput.value)
     const hourPlus = _vm.$el.querySelectorAll('td')[0].querySelector('button')
 
     // Due to the order in which the events are triggered by the browsers,
@@ -54,8 +54,7 @@ describe('TimePicker', () => {
       await vm.$nextTick()
       utils.triggerEvent(el, evtType)
       await vm.$nextTick()
-      afterText = hourInput.value
-      expect(parseInt(afterText)).to.equal(3)
+      expect(parseInt(hoursInput.value)).to.equal(3)
     }
 
     const testHourForQueuedEvents = async (el, firstEvtType, ticks, secondEvtType) => {
@@ -66,26 +65,25 @@ describe('TimePicker', () => {
       await utils.sleep(ticks * stepDelay)
       utils.triggerEvent(el, secondEvtType)
       await vm.$nextTick()
-      afterText = hourInput.value
-      expect(parseInt(afterText)).to.equal(2 + ticks)
+      expect(parseInt(hoursInput.value)).to.equal(2 + ticks)
     }
 
     // click hourPlus
     utils.triggerEvent(hourPlus, 'click')
     await vm.$nextTick()
-    let afterText = hourInput.value
-    if (parseInt(beforeText) !== 12) {
-      expect(parseInt(afterText)).to.equal(parseInt(beforeText) + 1)
+    let afterValue = parseInt(hoursInput.value)
+    if (beforeValue !== 12) {
+      expect(afterValue).to.equal(beforeValue + 1)
     } else {
-      expect(parseInt(afterText)).to.equal(1)
+      expect(afterValue).to.equal(1)
     }
     _vm.time.setHours(23)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
     utils.triggerEvent(hourPlus, 'click')
     await vm.$nextTick()
-    afterText = hourInput.value
-    expect(parseInt(afterText)).to.equal(12)
+    afterValue = parseInt(hoursInput.value)
+    expect(afterValue).to.equal(12)
 
     // mouseup hourPlus
     await testHourForSingleEvent(hourPlus, 'mouseup')
@@ -109,143 +107,146 @@ describe('TimePicker', () => {
     _vm.time.setHours(2)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    utils.triggerKey(hourInput, utils.keyCodes.up, 'down')
+    utils.triggerKey(hoursInput, utils.keyCodes.up, 'down')
     await utils.sleep(stepDelay)
-    utils.triggerKey(hourInput, utils.keyCodes.up, 'up')
+    utils.triggerKey(hoursInput, utils.keyCodes.up, 'up')
     await vm.$nextTick()
-    afterText = hourInput.value
-    expect(parseInt(afterText)).to.equal(3)
+    afterValue = parseInt(hoursInput.value)
+    expect(afterValue).to.equal(3)
 
     // Wheel
     _vm.time.setHours(2)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    utils.triggerEvent(hourInput, 'wheel', {deltaY: -1})
+    utils.triggerEvent(hoursInput, 'wheel', {deltaY: -1})
     await vm.$nextTick()
-    afterText = hourInput.value
-    expect(parseInt(afterText)).to.equal(3)
+    afterValue = parseInt(hoursInput.value)
+    expect(afterValue).to.equal(3)
   })
 
-  it('should be able to add 1 minutes', async () => {
-    let _vm = vm.$refs['time-picker-example']
+  it('should be able to add 1 minute', async () => {
+    const _vm = vm.$refs['time-picker-example']
     _vm.minStep = 1
     _vm.isReadOnly = false
     await vm.$nextTick()
-    let beforeText = _vm.$el.querySelectorAll('input')[1].value
-    let minutesPlus = _vm.$el.querySelectorAll('tr td button')[1]
-    minutesPlus.click()
+    const minuteInput = _vm.$el.querySelectorAll('input')[1]
+    const beforeValue = parseInt(minuteInput.value)
+    const minutesPlus = _vm.$el.querySelectorAll('tr td button')[1]
+    utils.triggerEvent(minutesPlus, 'click')
     await vm.$nextTick()
-    let afterText = _vm.$el.querySelectorAll('input')[1].value
-    if (parseInt(beforeText) !== 59) {
-      expect(parseInt(afterText)).to.equal(parseInt(beforeText) + 1)
+    const afterValue = parseInt(minuteInput.value)
+    if (beforeValue !== 59) {
+      expect(afterValue).to.equal(beforeValue + 1)
     } else {
-      expect(parseInt(afterText)).to.equal(0)
+      expect(afterValue).to.equal(0)
     }
   })
 
-  it('should be able to minus 1 hours', async () => {
-    let _vm = vm.$refs['time-picker-example']
+  it('should be able to minus 1 hour', async () => {
+    const _vm = vm.$refs['time-picker-example']
     _vm.hourStep = 1
     _vm.isReadOnly = false
     await vm.$nextTick()
-    let beforeText = _vm.$el.querySelectorAll('input')[0].value
-    let hourMinus = _vm.$el.querySelectorAll('tr')[2].querySelector('td button')
-    hourMinus.click()
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const beforeValue = parseInt(hoursInput.value)
+    const hourMinus = _vm.$el.querySelectorAll('tr')[2].querySelector('td button')
+    utils.triggerEvent(hourMinus, 'click')
     await vm.$nextTick()
-    let afterText = _vm.$el.querySelectorAll('input')[0].value
-    if (parseInt(beforeText) !== 1) {
-      expect(parseInt(afterText)).to.equal(parseInt(beforeText) - 1)
+    let afterValue = parseInt(hoursInput.value)
+    if (beforeValue !== 1) {
+      expect(afterValue).to.equal(beforeValue - 1)
     } else {
-      expect(parseInt(afterText)).to.equal(12)
+      expect(afterValue).to.equal(12)
     }
     _vm.time.setHours(0)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    hourMinus.click()
+    utils.triggerEvent(hourMinus, 'click')
     await vm.$nextTick()
-    afterText = _vm.$el.querySelectorAll('input')[0].value
-    expect(parseInt(afterText)).to.equal(11)
+    afterValue = parseInt(hoursInput.value)
+    expect(afterValue).to.equal(11)
   })
 
-  it('should be able to minus 1 minutes', async () => {
-    let _vm = vm.$refs['time-picker-example']
+  it('should be able to minus 1 minute', async () => {
+    const _vm = vm.$refs['time-picker-example']
     _vm.minStep = 1
     _vm.isReadOnly = false
     await vm.$nextTick()
-    let beforeText = _vm.$el.querySelectorAll('input')[1].value
-    let minutesMinus = _vm.$el.querySelectorAll('tr')[2].querySelectorAll('td button')[1]
-    minutesMinus.click()
+    const minuteInput = _vm.$el.querySelectorAll('input')[1]
+    const beforeValue = parseInt(minuteInput.value)
+    const minutesMinus = _vm.$el.querySelectorAll('tr')[2].querySelectorAll('td button')[1]
+    utils.triggerEvent(minutesMinus, 'click')
     await vm.$nextTick()
-    let afterText = _vm.$el.querySelectorAll('input')[1].value
-    if (parseInt(beforeText) !== 0) {
-      expect(parseInt(afterText)).to.equal(parseInt(beforeText) - 1)
+    const afterValue = parseInt(minuteInput.value)
+    if (beforeValue !== 0) {
+      expect(afterValue).to.equal(beforeValue - 1)
     } else {
-      expect(parseInt(afterText)).to.equal(59)
+      expect(afterValue).to.equal(59)
     }
   })
 
-  it('should be able to use 24H mode', async () => {
-    let _vm = vm.$refs['time-picker-24-example']
+  it('should be able to use 24h mode', async () => {
+    const _vm = vm.$refs['time-picker-24-example']
     await vm.$nextTick()
-    let toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
+    const toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
     expect(toggleBtn).not.exist
   })
 
   it('should be able to display correctly when hour = 0', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(0)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourText = _vm.$el.querySelectorAll('input')[0].value
-    let toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
-    expect(parseInt(hourText)).to.equal(12)
+    const hourValue = parseInt(_vm.$el.querySelectorAll('input')[0].value)
+    const toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
+    expect(hourValue).to.equal(12)
     expect(toggleBtn.textContent).to.equal('AM')
   })
 
   it('should be able to display correctly when hour = 12', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(12)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourText = _vm.$el.querySelectorAll('input')[0].value
-    let toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
-    expect(parseInt(hourText)).to.equal(12)
+    const hourValue = parseInt(_vm.$el.querySelectorAll('input')[0].value)
+    const toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
+    expect(hourValue).to.equal(12)
     expect(toggleBtn.textContent).to.equal('PM')
   })
 
   it('should be able to set hour using input in 24h mode', async () => {
-    let _vm = vm.$refs['time-picker-24-example']
+    const _vm = vm.$refs['time-picker-24-example']
     _vm.time.setHours(12)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourInput = _vm.$el.querySelectorAll('input')[0]
-    hourInput.value = 12
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    hoursInput.value = 12
     await vm.$nextTick()
-    expect(parseInt(hourInput.value)).to.equal(12)
-    hourInput.value = 13
+    expect(parseInt(hoursInput.value)).to.equal(12)
+    hoursInput.value = 13
     await vm.$nextTick()
-    expect(parseInt(hourInput.value)).to.equal(13)
+    expect(parseInt(hoursInput.value)).to.equal(13)
   })
 
   it('should be able to set hour using input in 12h mode', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(12)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourInput = _vm.$el.querySelectorAll('input')[0]
-    expect(parseInt(hourInput.value)).to.equal(12)
-    hourInput.value = 5
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    expect(parseInt(hoursInput.value)).to.equal(12)
+    hoursInput.value = 5
     await vm.$nextTick()
-    expect(parseInt(hourInput.value)).to.equal(5)
+    expect(parseInt(hoursInput.value)).to.equal(5)
   })
 
   it('should be able to set minute using input', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(12)
     _vm.time.setMinutes(0)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let minuteInput = _vm.$el.querySelectorAll('input')[1]
+    const minuteInput = _vm.$el.querySelectorAll('input')[1]
     expect(parseInt(minuteInput.value)).to.equal(0)
     minuteInput.value = 5
     await vm.$nextTick()
@@ -253,83 +254,88 @@ describe('TimePicker', () => {
   })
 
   it('should add hour when minute is 60', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(9)
     _vm.time.setMinutes(59)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let beforeHourText = _vm.$el.querySelectorAll('input')[0].value
-    let minutesPlus = _vm.$el.querySelectorAll('tr td button')[1]
-    minutesPlus.click()
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const minutesInput = _vm.$el.querySelectorAll('input')[1]
+    const beforeHoursValue = parseInt(hoursInput.value)
+    const minutesPlus = _vm.$el.querySelectorAll('tr td button')[1]
+    utils.triggerEvent(minutesPlus, 'click')
     await vm.$nextTick()
-    let afterHourText = _vm.$el.querySelectorAll('input')[0].value
-    let afterMinutesText = _vm.$el.querySelectorAll('input')[1].value
-    expect(parseInt(afterHourText)).to.equal(parseInt(beforeHourText) + 1)
-    expect(parseInt(afterMinutesText)).to.equal(0)
+    const afterHoursValue = parseInt(hoursInput.value)
+    const afterMinutesValue = parseInt(minutesInput.value)
+    expect(afterHoursValue).to.equal(beforeHoursValue + 1)
+    expect(afterMinutesValue).to.equal(0)
   })
 
   it('should minus hour when minute is -1', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(9)
     _vm.time.setMinutes(0)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let beforeHourText = _vm.$el.querySelectorAll('input')[0].value
-    let minutesMinus = _vm.$el.querySelectorAll('tr')[2].querySelectorAll('td button')[1]
-    minutesMinus.click()
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const beforeHoursValue = parseInt(hoursInput.value)
+    const minutesMinus = _vm.$el.querySelectorAll('tr')[2].querySelectorAll('td button')[1]
+    utils.triggerEvent(minutesMinus, 'click')
     await vm.$nextTick()
-    let afterHourText = _vm.$el.querySelectorAll('input')[0].value
-    let afterMinutesText = _vm.$el.querySelectorAll('input')[1].value
-    expect(parseInt(afterHourText)).to.equal(parseInt(beforeHourText) - 1)
-    expect(parseInt(afterMinutesText)).to.equal(59)
+    const afterHoursValue = parseInt(hoursInput.value)
+    const afterMinutesText = parseInt(_vm.$el.querySelectorAll('input')[1].value)
+    expect(afterHoursValue).to.equal(beforeHoursValue - 1)
+    expect(afterMinutesText).to.equal(59)
   })
 
   it('can be set to 9:00', async () => {
-    let _vm = vm.$refs['time-picker-example']
+    const _vm = vm.$refs['time-picker-example']
     _vm.time.setHours(9)
     _vm.time.setMinutes(0)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourText = _vm.$el.querySelectorAll('input')[0].value
-    let minutesText = _vm.$el.querySelectorAll('input')[1].value
-    let toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
-    expect(hourText).to.equal('09')
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const minutesInput = _vm.$el.querySelectorAll('input')[1]
+    const hoursText = hoursInput.value
+    const minutesText = minutesInput.value
+    const toggleBtn = _vm.$el.querySelector('[data-action="toggleMeridian"]')
+    expect(hoursText).to.equal('09')
     expect(minutesText).to.equal('00')
     expect(toggleBtn.textContent).to.equal('AM')
   })
 
   it('should not be able to update time bigger than max', async () => {
-    let _vm = vm.$refs['time-picker-limit-example']
+    const _vm = vm.$refs['time-picker-limit-example']
     _vm.time.setHours(20)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourText = _vm.$el.querySelectorAll('input')[0]
-    let hourPlus = _vm.$el.querySelectorAll('td')[0].querySelector('button')
-    expect(hourText.value).to.equal('08')
-    hourPlus.click()
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    const hourPlus = _vm.$el.querySelectorAll('td')[0].querySelector('button')
+    expect(hoursInput.value).to.equal('08')
+    utils.triggerEvent(hourPlus, 'click')
     await vm.$nextTick()
-    expect(hourText.value).to.equal('08')
+    expect(hoursInput.value).to.equal('08')
   })
 
   it('should not be able to update time smaller than min', async () => {
-    let _vm = vm.$refs['time-picker-limit-example']
+    const _vm = vm.$refs['time-picker-limit-example']
     _vm.time.setHours(8)
     _vm.time.setMinutes(30)
     _vm.time = new Date(_vm.time)
     await vm.$nextTick()
-    let hourText = _vm.$el.querySelectorAll('input')[0]
-    expect(hourText.value).to.equal('08')
-    let hourMinus = _vm.$el.querySelectorAll('tr')[2].querySelector('td button')
-    hourMinus.click()
+    const hoursInput = _vm.$el.querySelectorAll('input')[0]
+    expect(hoursInput.value).to.equal('08')
+    const hourMinus = _vm.$el.querySelectorAll('tr')[2].querySelector('td button')
+    utils.triggerEvent(hourMinus, 'click')
     await vm.$nextTick()
-    let minutesText = _vm.$el.querySelectorAll('input')[1]
-    expect(hourText.value).to.equal('08')
-    expect(minutesText.value).to.equal('00')
+    const minutesInput = _vm.$el.querySelectorAll('input')[1]
+    expect(hoursInput.value).to.equal('08')
+    expect(minutesInput.value).to.equal('00')
   })
 
   it('should be able to use custom icons', async () => {
-    let _vm = vm.$refs['time-picker-icons-example']
-    let $el = $(_vm.$el)
+    const _vm = vm.$refs['time-picker-icons-example']
+    const $el = $(_vm.$el)
     expect($el.find('tr:first-child .btn > i').get(0).className).to.contain('glyphicon-plus')
     expect($el.find('tr:last-child .btn > i').get(0).className).to.contain('glyphicon-minus')
   })
