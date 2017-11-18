1327f222765008e820c7898fbba84b362c634a10
wxsm <wxsms@foxmail.com>
2017-11-19 00:22:25 +0800

    add ga

--- docs/main.js
+++ docs/main.js
@@ -6,6 +6,7 @@ import './assets/css/common.less'
 import 'es6-promise/auto'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 import router from './router'
 import PageWrapper from './components/architecture/PageWrapper.vue'
 import MarkdownWrapper from './components/architecture/MarkdownWrapper.vue'
@@ -15,6 +16,11 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(uiv)
+Vue.use(VueAnalytics, {
+  id: 'UA-109876095-1',
+  router
+})
+
 Vue.component('MarkdownWrapper', MarkdownWrapper)
 
 document.addEventListener('DOMContentLoaded', () => {
