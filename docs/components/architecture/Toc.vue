d083f38b964c2c90517af91ee39d078779b7bf33
wxsm <wxsms@foxmail.com>
2017-11-27 19:06:48 +0800

    better document toc active style

--- docs/components/architecture/Toc.vue
+++ docs/components/architecture/Toc.vue
@@ -28,34 +28,47 @@
     list-style: none;
     padding: 0;
 
-    ul {
-      list-style: none;
-      padding-left: 0;
-    }
-
     li {
       margin: 5px 0;
 
       &.active {
         > a {
           color: @highlight-color !important;
+          box-shadow: -2px 0 0 @highlight-color;
+        }
+      }
+
+      > a {
+        padding-left: 10px;
+        font-size: 1.1em;
+      }
+
+      a {
+        color: #99979c !important;
+        text-decoration: none !important;
+        display: block;
+
+        &:hover {
+          color: @highlight-color !important;
+        }
+
+        &.back-to-top {
+          opacity: .8;
+          margin-top: 20px;
+          font-size: .9em;
         }
       }
     }
 
-    a {
-      color: #99979c !important;
-      text-decoration: none !important;
-      display: block;
+    ul {
+      list-style: none;
+      padding-left: 0;
 
-      &:hover {
-        color: @highlight-color !important;
-      }
-
-      &.back-to-top {
-        opacity: .8;
-        margin-top: 20px;
-        font-size: 90%;
+      li {
+        > a {
+          font-size: 1em;
+          padding-left: 20px;
+        }
       }
     }
   }
