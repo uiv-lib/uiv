948bb041cda127170fd26847bc3ee2e1efe84126
Raffaele Salmaso <raffaele@salmaso.org>
2017-11-27 16:40:02 +0100

    added italian translations

---
+++ src/locale/lang/it-IT.js
@@ -0,0 +1,37 @@
+export default {
+  uiv: {
+    datePicker: {
+      clear: 'Pulisci',
+      today: 'Oggi',
+      month: 'Mese',
+      month1: 'Gennaio',
+      month2: 'Febbraio',
+      month3: 'Marzo',
+      month4: 'Aprile',
+      month5: 'Maggio',
+      month6: 'Giugno',
+      month7: 'Luglio',
+      month8: 'Agosto',
+      month9: 'Settembre',
+      month10: 'Ottobre',
+      month11: 'Novembre',
+      month12: 'Dicembre',
+      year: 'Anno',
+      week1: 'Lun',
+      week2: 'Mar',
+      week3: 'Mer',
+      week4: 'Gio',
+      week5: 'Ven',
+      week6: 'Sab',
+      week7: 'Dom'
+    },
+    timePicker: {
+      am: 'AM',
+      pm: 'PM'
+    },
+    modal: {
+      cancel: 'Annulla',
+      ok: 'OK'
+    }
+  }
+}
