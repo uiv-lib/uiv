cf838a9e658df99bb9bbf34265854e9b360ae9dc
Jaakko Rasmus <jaakko.rasmus@gmail.com>
2018-03-10 17:02:31 +0200

    Added Finnish and Swedish locale

---
+++ src/locale/lang/fi-Fi.js
@@ -0,0 +1,37 @@
+export default {
+  uiv: {
+    datePicker: {
+      clear: 'Tyhjennä',
+      today: 'Tänään',
+      month: 'Kuukausi',
+      month1: 'Tammikuu',
+      month2: 'Helmikuu',
+      month3: 'Maaliskuu',
+      month4: 'Huhtikuu',
+      month5: 'Toukokuu',
+      month6: 'Kesäkuu',
+      month7: 'Heinäkuu',
+      month8: 'Elokuu',
+      month9: 'Syyskuu',
+      month10: 'Lokakuu',
+      month11: 'Marraskuu',
+      month12: 'Joulukuu',
+      year: 'Vuosi',
+      week1: 'Ma',
+      week2: 'Ti',
+      week3: 'Ke',
+      week4: 'To',
+      week5: 'Pe',
+      week6: 'La',
+      week7: 'Su'
+    },
+    timePicker: {
+      am: 'AM',
+      pm: 'PM'
+    },
+    modal: {
+      cancel: 'Peruuta',
+      ok: 'OK'
+    }
+  }
+}
