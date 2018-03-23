adbb529dca9d601442f52e7eddbe74d3b235febe
Alvaro <munozcotelo@users.noreply.github.com>
2018-03-23 03:34:35 +0100

    Create es-ES.js (#193)
    Support es-ES language

---
+++ src/locale/lang/es-ES.js
@@ -0,0 +1,37 @@
+export default {
+  uiv: {
+    datePicker: {
+      clear: 'Limpiar',
+      today: 'Hoy',
+      month: 'Mes',
+      month1: 'Enero',
+      month2: 'Febrero',
+      month3: 'Marzo',
+      month4: 'Abril',
+      month5: 'Mayo',
+      month6: 'Junio',
+      month7: 'Julio',
+      month8: 'Agosto',
+      month9: 'Septiembre',
+      month10: 'Octubre',
+      month11: 'Noviembre',
+      month12: 'Diciembre',
+      year: 'Año',
+      week1: 'Lunes',
+      week2: 'Martes',
+      week3: 'Miercoles',
+      week4: 'Jueves',
+      week5: 'Viernes',
+      week6: 'Sabado',
+      week7: 'Domingo'
+    },
+    timePicker: {
+      am: 'AM',
+      pm: 'PM'
+    },
+    modal: {
+      cancel: 'Cancelar',
+      ok: 'OK'
+    }
+  }
+}
