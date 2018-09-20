7a81e691b9f60a3381bd7ea9be4543476e385fa2
wxsm <wxsms@foxmail.com>
2018-09-20 12:44:28 +0800

    [DatePicker] fixed today btn not work if today is already selected (close #252)

--- src/components/datepicker/DatePicker.vue
+++ src/components/datepicker/DatePicker.vue
@@ -210,6 +210,10 @@
         if (date && isNumber(date.date) && isNumber(date.month) && isNumber(date.year)) {
           let _date = new Date(date.year, date.month, date.date)
           this.$emit('input', stringify(_date, this.format))
+          // if the input event trigger nothing (same value)
+          // manually correct
+          this.currentMonth = date.month
+          this.currentYear = date.year
         } else {
           this.$emit('input', '')
         }
