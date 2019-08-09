314fa39e688230c97b1a7a7d7aad83f676f56fa8
Jesper Høy <jesperhoy@users.noreply.github.com>
2019-08-09 22:32:09 +0200

    Danish localization

---
+++ src/locale/lang/da-DK.js
@@ -0,0 +1,37 @@
+export default {
+  uiv: {
+    datePicker: {
+      clear: 'Nulstil',
+      today: 'I dag',
+      month: 'Måned',
+      month1: 'Januar',
+      month2: 'Februar',
+      month3: 'Marts',
+      month4: 'April',
+      month5: 'Maj',
+      month6: 'Juni',
+      month7: 'Juli',
+      month8: 'August',
+      month9: 'September',
+      month10: 'Oktober',
+      month11: 'November',
+      month12: 'December',
+      year: 'År',
+      week1: 'Ma',
+      week2: 'Ti',
+      week3: 'On',
+      week4: 'To',
+      week5: 'Fr',
+      week6: 'Lø',
+      week7: 'Sø'
+    },
+    timePicker: {
+      am: 'Formiddag',
+      pm: 'Eftermiddag'
+    },
+    modal: {
+      cancel: 'Annuller',
+      ok: 'OK'
+    }
+  }
+}
