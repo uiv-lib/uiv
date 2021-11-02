4b4ae64276e00e1589ddc7a04d9c75fb0c1408ac
wxsm <wxsms@foxmail.com>
2021-11-02 20:31:58 +0800

    test

--- build/rollup.umd.js
+++ build/rollup.umd.js
@@ -7,7 +7,7 @@ const baseConfig = require('./rollup.base')
 const { name } = require('../package.json')
 
 module.exports = _.merge({}, baseConfig, {
-  input: path.join(__dirname, '..', 'src', 'index-browser.js'),
+  input: path.join(__dirname, '..', 'src', 'index.browser.js'),
   plugins: [uglify(), filesize()],
   output: {
     format: 'umd',
