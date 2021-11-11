7edf4a6a2bef417b691445be580cffae7cc8bd65
guokairui <wxsms@foxmail.com>
2021-11-11 11:45:59 +0800

    vue3

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
+import * as uiv from '../../../src/index'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
+    app.use(uiv)
     // register global components
     // app.component('MyGlobalComponent' /* ... */)
   }
