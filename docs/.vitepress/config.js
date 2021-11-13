acc89f3e9ed9357e23a28949169f9892fc25d013
wxsm <wxsms@foxmail.com>
2021-11-13 13:21:38 +0800

    vue3

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,63 +16,8 @@ export default {
           'Bootstrap,Vue,JavaScript,HTML,CSS,Components,Directives,UI,Affix,MessageBox,Notification,ScrollSpy,Alert,Button,ButtonGroup,Breadcrumbs,Carousel,Collapse,DatePicker,Dropdown,Modal,Pagination,Popover,ProgressBar,Tabs,TimePicker,Tooltip,Typeahead,AutoComplete,MultiSelect,Navbar',
       },
     ],
-    // ['script', {
-    //   'data-ad-client': 'ca-pub-4714899946256166',
-    //   async: true,
-    //   src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-    // }]
   ],
   title: 'uiv',
-  // plugins: {
-  //   '@vuepress/active-header-links': null,
-  //   '@vuepress/google-analytics': {
-  //     ga: 'UA-102731925-2',
-  //   },
-  //   sitemap: {
-  //     hostname: 'https://uiv.wxsm.space',
-  //     dateFormatter: (time) => {
-  //       // chinese date won't work here
-  //       try {
-  //         return new Date(time).toISOString()
-  //       } catch (err) {
-  //         return new Date().toISOString()
-  //       }
-  //     },
-  //   },
-  //   redirect: {
-  //     redirectors: [
-  //       {
-  //         base: '/usage/',
-  //         alternative: ['getting-started'],
-  //       },
-  //       {
-  //         base: '/components/',
-  //         alternative: ['btn'],
-  //       },
-  //       {
-  //         base: '/zh/usage/',
-  //         alternative: ['getting-started'],
-  //       },
-  //       {
-  //         base: '/zh/components/',
-  //         alternative: ['btn'],
-  //       },
-  //     ],
-  //   },
-  // },
-  // locales: {
-  //   '/': {
-  //     lang: 'en-US',
-  //   },
-  //   '/zh/': {
-  //     lang: 'zh-CN',
-  //     title: 'uiv',
-  //     description: '基于 Vue 2 的 Bootstrap 3 组件库',
-  //   },
-  // },
-  // markdown: {
-  //   toc: { includeLevel: [1, 2] },
-  // },
   themeConfig: {
     logo: '/assets/image/logo.png',
     lastUpdated: 'Last Updated',
@@ -81,103 +26,98 @@ export default {
     repoLabel: 'GitHub',
     docsRepo: 'uiv-lib/uiv',
     docsDir: 'docs',
-    docsBranch: 'master',
+    docsBranch: 'dev',
     editLinks: true,
     editLinkText: 'Edit this page on GitHub',
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Usage', link: '/usage/getting-started' },
-      { text: 'Components', link: '/components/btn' },
-      { text: 'Funding', link: '/funding/' },
+      {
+        text: 'Usage',
+        link: '/usage/getting-started',
+        activeMatch: '^/usage/',
+      },
+      {
+        text: 'Components',
+        link: '/components/btn',
+        activeMatch: '^/components/',
+      },
+      { text: '1.x', link: 'https://uiv-v1.wxsm.space' },
       { text: '0.x', link: 'https://uiv-v0.wxsm.space' },
+      { text: 'Funding', link: '/funding/' },
       { text: 'Changelog', link: 'https://github.com/uiv-lib/uiv/releases' },
     ],
     sidebar: {
-      '/': [
+      '/usage': [
+        { text: 'Getting Started', link: '/usage/getting-started' },
         {
-          text: 'Usage',
+          text: 'i18n',
+          link: '/usage/i18n',
+        },
+      ],
+      '/components': [
+        {
+          text: 'Basic',
           collapsable: false,
           children: [
-            { text: 'Getting Started', link: '/usage/getting-started' },
-            {
-              text: 'i18n',
-              link: '/usage/i18n',
-            },
+            { text: 'Button', link: '/components/btn' },
+            { text: 'Button Group', link: '/components/btn-group' },
+            { text: 'Collapse', link: '/components/collapse' },
           ],
         },
         {
-          text: 'Components',
+          text: 'Popup',
           collapsable: false,
           children: [
-            {
-              text: 'Basic',
-              collapsable: false,
-              children: [
-                { text: 'btn', link: '/components/btn' },
-                { text: 'btn-group', link: '/components/btn-group' },
-                { text: 'collapse', link: '/components/collapse' },
-              ],
-            },
-            {
-              text: 'Popup',
-              collapsable: false,
-              children: [
-                { text: 'dropdown', link: '/components/dropdown' },
-                { text: 'modal', link: '/components/modal' },
-                { text: 'tooltip', link: '/components/tooltip' },
-                { text: 'popover', link: '/components/popover' },
-              ],
-            },
-            {
-              text: 'Form',
-              collapsable: false,
-              children: [
-                { text: 'multi-select', link: '/components/multi-select' },
-                { text: 'typeahead', link: '/components/typeahead' },
-                { text: 'date-picker', link: '/components/date-picker' },
-                { text: 'time-picker', link: '/components/time-picker' },
-              ],
-            },
-            {
-              text: 'Notice',
-              collapsable: false,
-              children: [
-                { text: 'alert', link: '/components/alert' },
-                { text: 'notification', link: '/components/notification' },
-                { text: 'message-box', link: '/components/message-box' },
-              ],
-            },
-            {
-              text: 'Navigation',
-              collapsable: false,
-              children: [
-                { text: 'navbar', link: '/components/navbar' },
-                { text: 'tabs', link: '/components/tabs' },
-                { text: 'breadcrumbs', link: '/components/breadcrumbs' },
-              ],
-            },
-            {
-              text: 'Indicator',
-              collapsable: false,
-              children: [
-                { text: 'pagination', link: '/components/pagination' },
-                { text: 'progress-bar', link: '/components/progress-bar' },
-              ],
-            },
-            {
-              text: 'Others',
-              collapsable: false,
-              children: [
-                { text: 'carousel', link: '/components/carousel' },
-                { text: 'affix', link: '/components/affix' },
-                { text: 'scroll-spy', link: '/components/scroll-spy' },
-              ],
-            },
+            { text: 'Dropdown', link: '/components/dropdown' },
+            { text: 'Modal', link: '/components/modal' },
+            { text: 'Tooltip', link: '/components/tooltip' },
+            { text: 'Popover', link: '/components/popover' },
           ],
         },
         {
-          text: 'Funding',
-          link: '/funding/',
+          text: 'Form',
+          collapsable: false,
+          children: [
+            { text: 'Multi Select', link: '/components/multi-select' },
+            { text: 'Typeahead', link: '/components/typeahead' },
+            { text: 'Date Picker', link: '/components/date-picker' },
+            { text: 'Time Picker', link: '/components/time-picker' },
+          ],
+        },
+        {
+          text: 'Notice',
+          collapsable: false,
+          children: [
+            { text: 'Alert', link: '/components/alert' },
+            { text: 'Notification', link: '/components/notification' },
+            { text: 'Message Box', link: '/components/message-box' },
+          ],
+        },
+        {
+          text: 'Navigation',
+          collapsable: false,
+          children: [
+            { text: 'Navbar', link: '/components/navbar' },
+            { text: 'Tabs', link: '/components/tabs' },
+            { text: 'Breadcrumbs', link: '/components/breadcrumbs' },
+          ],
+        },
+        {
+          text: 'Indicator',
+          collapsable: false,
+          children: [
+            { text: 'Pagination', link: '/components/pagination' },
+            { text: 'Progress Bar', link: '/components/progress-bar' },
+          ],
+        },
+        {
+          text: 'Others',
+          collapsable: false,
+          children: [
+            { text: 'Carousel', link: '/components/carousel' },
+            { text: 'Affix', link: '/components/affix' },
+            { text: 'Scroll Spy', link: '/components/scroll-spy' },
+          ],
         },
       ],
     },
