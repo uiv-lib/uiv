58e40a90f4d122bd5e613019ae82fcad5928d3eb
guokairui <wxsms@foxmail.com>
2021-11-15 19:40:39 +0800

    chore: single build script with vite

---
+++ build/single.js
@@ -0,0 +1,64 @@
+const { exec } = require('child_process')
+
+const files = [
+  'services/notification/Notification.js',
+  'services/messagebox/MessageBox.js',
+  'directives/popover/popover.js',
+  'directives/scrollspy/scrollspy.js',
+  'directives/tooltip/tooltip.js',
+  'components/affix/Affix.vue',
+  'components/alert/Alert.vue',
+  'components/breadcrumbs/BreadcrumbItem.vue',
+  'components/breadcrumbs/Breadcrumbs.vue',
+  'components/button/Btn.vue',
+  'components/button/BtnGroup.vue',
+  'components/button/BtnToolbar.vue',
+  'components/carousel/Carousel.vue',
+  'components/carousel/Slide.vue',
+  'components/collapse/Collapse.js',
+  'components/datepicker/DatePicker.vue',
+  'components/dropdown/Dropdown.vue',
+  'components/modal/Modal.vue',
+  'components/navbar/Navbar.vue',
+  'components/navbar/NavbarForm.vue',
+  'components/navbar/NavbarNav.vue',
+  'components/navbar/NavbarText.vue',
+  'components/pagination/Pagination.vue',
+  'components/popover/Popover.vue',
+  'components/progressbar/ProgressBar.vue',
+  'components/progressbar/ProgressBarStack.vue',
+  'components/select/MultiSelect.vue',
+  'components/tabs/Tab.vue',
+  'components/tabs/Tabs.vue',
+  'components/timepicker/TimePicker.vue',
+  'components/tooltip/Tooltip.vue',
+  'components/typeahead/Typeahead.vue',
+]
+
+function e(command, options) {
+  return new Promise((resolve, reject) => {
+    exec(command, options, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        reject(error)
+      }
+      console.log(`stdout: ${stdout}`)
+      console.error(`stderr: ${stderr}`)
+      resolve()
+    })
+  })
+}
+
+;(async function () {
+  for (const file of files) {
+    const prefix = file.startsWith('directives') ? 'v_' : ''
+
+    await e('npx vite build -c build/vite.config.js', {
+      env: {
+        ...process.env,
+        UIV_ENTRY: file,
+        UIV_FILENAME: `${prefix + file.split('/').pop().split('.')[0]}.js`,
+      },
+    })
+  }
+})()
--- build/vite.config.js
+++ build/vite.config.js
@@ -3,14 +3,20 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
+const { UIV_ENTRY, UIV_FILENAME } = process.env
+
+console.log('env:', UIV_ENTRY, UIV_FILENAME)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   build: {
+    emptyOutDir: false,
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
-      name: 'uiv',
-      formats: ['cjs', 'es', 'iife', 'umd'],
+      entry: resolve(__dirname, `../src/${UIV_ENTRY || 'index.js'}`),
+      name: UIV_ENTRY ? undefined : 'uiv',
+      formats: UIV_ENTRY ? ['es'] : ['cjs', 'es', 'iife', 'umd'],
+      fileName: (format) => (UIV_FILENAME ? UIV_FILENAME : `uiv.${format}.js`),
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
