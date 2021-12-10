66904bc9008118e54cef7a7101d92d92fba0cdbf
guokairui <wxsms@foxmail.com>
2021-12-10 14:56:44 +0800

    docs: fix typeahead custom template example

--- docs/.vitepress/components/typeahead/custom-template.vue
+++ docs/.vitepress/components/typeahead/custom-template.vue
@@ -14,15 +14,19 @@
       :async-function="queryFunction"
       item-key="login"
     >
-      <template #item="props">
+      <template #item="{ items, activeIndex, select, highlight }">
         <li
-          v-for="(item, index) in props.items"
-          :key="index"
-          :class="{ active: props.activeIndex === index }"
+          v-for="(item, index) in items"
+          :key="item.id"
+          :class="{ active: activeIndex === index }"
         >
-          <a role="button" @click="props.select(item)">
-            <img width="22px" height="22px" :src="item.avatar_url + '&s=40'" />
-            <span v-html="props.highlight(item)"></span>
+          <a role="button" @click="select(item)">
+            <img
+              style="width: 22px; height: 22px; margin-right: 5px"
+              :src="item.avatar_url + '&s=40'"
+              alt="avatar"
+            />
+            <span v-html="highlight(item)"></span>
           </a>
         </li>
       </template>
